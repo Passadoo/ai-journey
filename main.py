e80241ddfeb953c841693cbcaea4946492fad2db
Simon Borgmästars <simon.borgmastars@pm.me>
2026-02-01 13:02:08 +0100

    task 1.1

--- main.py
+++ main.py
@@ -24,4 +24,10 @@ for i in range(len(examples)):
     fig.add_subplot(2,2,i+1)
     plt.imshow(examples[i])
 
+# Setup labels - we want our machine to output positive voltage to T shapes,
+# our first 2 examples are Ts - so we'll set these values to +1, our second to
+# examples are Js - so we'll set these to -1s
+y=np.array([1,1,-1,-1])
+
+
 plt.show()
